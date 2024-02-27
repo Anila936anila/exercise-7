650631884ca4455294c1518786ae9fd08d8f5577
Anila936anila <161087832+Anila936anila@users.noreply.github.com>
2024-02-27 12:31:28 -0800

    Number 8
    Typescript 45Q assignment exercise 7

---
+++ index.js
@@ -0,0 +1,13 @@
+"use strict";
+//Addition : 
+const additionResult = 4 + 4;
+console.log("Addition Result:" + additionResult);
+// Subtraction
+const subtractionResult = 16 - 8;
+console.log("subtractionResult: " + subtractionResult);
+//Multiplication
+const multiplicationResult = 4 * 2;
+console.log("multiplication: " + multiplicationResult);
+// Division
+const divisionResult = 64 / 8;
+console.log("division Result: " + divisionResult);
